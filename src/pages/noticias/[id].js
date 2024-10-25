cc87badb3d1bf0b5610190fce2ecab64c15f0781
llekelly <119963791+llekelly@users.noreply.github.com>
2024-10-25 09:44:49 -0300

    Add files via upload

--- src/pages/noticias/[id].js
+++ src/pages/noticias/[id].js
@@ -1,7 +1,7 @@
-import {useRouter} from "next/router"
-export default function Page(){
+import { useRouter } from "next/router"
+export default function Page() {
     const router = useRouter()
     return <>
-    <p>Post: {router.query.id}</p>
+        <p>Post: {router.query.id}</p>
     </>
 }
